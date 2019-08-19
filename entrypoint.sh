4af1321fb766fe19e3f52aec7166b67e4169e6cc
Allen Day <allenday@allenday.com>
2019-08-19 12:27:54 +0000

    t

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-solr start && java -jar /uploadservlet/target/standalone.jar -httpPort=9999
+solr start && \
+java -jar /uploadservlet/target/standalone.jar -httpPort=9999
+#cp -prv /solr-data/* /var/solr/data/
+#sleep 20 && \
+#solr create_core -c frameshift -p 8983 && \
